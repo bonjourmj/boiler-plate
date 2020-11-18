9e521683eb72a06d937ef4b0ba12ffa59f79d3d3
PARK MINJI <mjmacpro@MJui-MacBookPro.local>
2020-11-18 17:04:53 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI // HEROKU에 저장된 Config Vars와 같게
+}
--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+
+// 서버에서 정보를 분석해서 가져옴
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://bonjourmj:ahdrhelql@boiler-plate.ugcjy.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', (req, res) => res.send('Hello World!')),
+
+// 회원가입을 위한 라우트
+app.post('/register', (req, res) => {
+
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 디비에 넣어준다
+
+  const user = new User(req.body)
+
+  // MongoDB에서 오는 메소드
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
